b4c9043bf281d475616d4e5a8d2ff079a000ae4c
Sean Cahill <seancahill25@gmail.com>
2017-11-29 04:54:05 +0000

    added descriptions and links to articles

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
 /*global $ APIKEY*/
 $(document).ready(function() {
-    $.ajax({
-        //   method: "GET",
-          url: "https://newsapi.org/v2/sources",
-          data: { category: "sport", country: "us", language: "en", apiKey: APIKEY},
-          success: function(data) {
-              if(data.status == "ok") {
-                   console.log(data)
-                   for (var i= 0; i < data.sources.length; i++) {
-                       var source = document.createElement("OPTION");
-                       source.setAttribute("value", data.sources[i].id)
-                       source.innerHTML = data.sources[i].name;
-                       document.getElementById('selection').appendChild(source);
-            }
-          }
-          function news(site) {
-           $.ajax({
-        url: "https://newsapi.org/v2/top-headlines?sources=" + site,
-        data: {apiKey: APIKEY},
-        success: function(articles) {
-          for  (var a= 0; a < articles.articles.length; a++) {
-              var headline = document.createElement("P")
-              headline.innerHTML = articles.articles[a].title;
-              document.getElementById("Article").appendChild(headline)
-           console.log(articles)
-          }
-        
-        }
-    })
-          }  $('#source').submit(function(event) {
-        event.preventDefault();
-        news(document.getElementById("selection").value)
-     });
-        }
-    })
-    
-    // .done(function( data ) {
-    //   console.log( data );
-    //   console.log( data.status);
-    // });
-   
+	$.ajax({
+		//   method: "GET",
+		url: "https://newsapi.org/v2/sources",
+		data: {
+			category: "sport",
+			country: "us",
+			language: "en",
+			apiKey: APIKEY
+		},
+		success: function(data) {
+			if (data.status == "ok") {
+				console.log(data)
+				for (var i = 0; i < data.sources.length; i++) {
+					var source = document.createElement("OPTION");
+					source.setAttribute("value", data.sources[i].id)
+					source.innerHTML = data.sources[i].name;
+					document.getElementById('selection').appendChild(source);
+				}
+			}
+
+			function news(site) {
+				$.ajax({
+					url: "https://newsapi.org/v2/top-headlines?sources=" + site,
+					data: {
+						apiKey: APIKEY
+					},
+					success: function(articles) {
+						for (var a = 0; a < articles.articles.length; a++) {
+							var headline = document.createElement("P")
+							var Anchor = document.createElement('a');
+							Anchor.href = articles.articles[a].url;
+							Anchor.innerHTML = articles.articles[a].title + "<br>";
+							headline.innerHTML = articles.articles[a].description;
+							document.getElementById("Article").appendChild(Anchor).appendChild(headline)
+							console.log(articles)
+						}
+					}
+				})
+			}
+			$('#source').submit(function(event) {
+				event.preventDefault();
+				news(document.getElementById("selection").value)
+			});
+		}
+	})
 })
